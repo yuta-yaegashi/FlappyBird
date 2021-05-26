0fae7cca997acc92ecfdd478ed9b3f283bdb3334
yuta.yaegashi <yuta090711@icloud.com>
2021-05-26 15:29:46 +0900

    アイテムを追加した。

--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //SKView型に変更する
+        let skView = self.view as! SKView
+        
+        // FPSを表示する
+        skView.showsFPS = true
+        
+        // ノードの数を表示する
+        skView.showsNodeCount = true
+        
+        // ビューと同じサイズでシーンを作成する
+        let scene = GameScene(size:skView.frame.size)
+        
+        // ビューにシーンを表示する
+        skView.presentScene(scene)
     }
-
-
+    // ステータスバーを消す 
+        override var prefersStatusBarHidden: Bool {
+            get {
+                return true
+            }
+        }
+    
 }
 
